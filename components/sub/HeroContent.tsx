8d723939d341d04c385ea5e018804fb807540ee3
prateek <prateeksingh8841@gmail.com>
2024-01-06 18:11:00 -0700

    feat: eslint

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -33,20 +33,20 @@ export const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Hi. It's
+            Hi. It&apos;s
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
               Prateek
             </span>
-          .{/* project experience */}
+            .{/* project experience */}
           </span>
         </motion.div>
         <motion.p
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Developer with experience in Web,
-          Mobile and Software Engineering. Checkout my projects and skills.
+          I&apos;m a Full Stack Developer with experience in Web, Mobile and
+          Software Engineering. Checkout my projects and skills.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
