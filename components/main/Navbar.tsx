cf78f7109830ccfa5c6da1bc555be90c9cfef998
prateek <prateeksingh8841@gmail.com>
2024-01-07 14:34:39 -0700

    feat: some changes

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-[10px]">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto">
         <a
-          href="#about-me"
+          href="#about"
           className="h-auto w-auto flex flex-row items-center"
         >
           {/* <Image
@@ -33,7 +33,7 @@ const Navbar = () => {
 
         <div className="w-[500px] h-full flex flex-row items-center">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
+            <a href="#about" className="cursor-pointer">
               About
             </a>
             <a href="#skills" className="cursor-pointer">
