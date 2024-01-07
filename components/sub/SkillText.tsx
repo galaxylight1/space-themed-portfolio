94132a755479c005008fa5183e680b924c0a722f
prateek <prateeksingh8841@gmail.com>
2024-01-06 23:38:54 -0700

    feat: some more changes

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -24,7 +24,7 @@ const SkillText = () => {
         variants={slideInFromLeft(0.5)}
         className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
       >
-        Technical Skills
+        Skills
       </motion.div>
       <motion.div
         variants={slideInFromRight(0.5)}
